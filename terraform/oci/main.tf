68e549d6736a219088655c1340fce7b3dca5facd
vellfire <will@mcda.dev>
2025-05-20 01:18:21 +0100

    chore(pods): fix unifi controller and change paths

--- terraform/oci/main.tf
+++ terraform/oci/main.tf
@@ -1,1 +1,12 @@
+resource "oci_core_vcn" "vcn1" {
+  cidr_block = "10.0.0.0/16"
+  dns_label = "vcn1"
+  compartment_id = var.compartment_id
+  display_name ="vcn1"
+}
 
+resource "oci_core_internet_gateway" "inetGw" {
+  compartment_id = var.compartment_id
+  display_name = "inetGw"
+  vcn_id = oci_core_vcn.vcn1.id
+}
