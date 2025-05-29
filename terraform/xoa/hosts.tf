d8dac9c3267b146cdb44a9296909673d32a2d446
vellfire <will@mcda.dev>
2025-05-29 03:06:34 +0100

    feat(ansible-pods): add iplayarr pod configuration feat(ansible-pods): add user settings to dl1 socks5 proxy chore(ansible-inv): remove kvm1 from inventory groups feat(tf-xoa): add m720q xoa pool and network configurations

--- terraform/xoa/hosts.tf
+++ terraform/xoa/hosts.tf
@@ -1,3 +1,4 @@
+# xng1
 data "xenorchestra_pool" "xng1" {
   name_label = "xng1"
 }
@@ -10,10 +11,6 @@ data "xenorchestra_sr" "xng1" {
 data "xenorchestra_network" "xng1" {
   name_label = "Pool-wide network associated with eth0"
   pool_id    = data.xenorchestra_pool.xng1.id
-}
-
-data "xenorchestra_template" "opnsense_template" {
-  name_label = "OPNsense_template"
 }
 
 resource "xenorchestra_network" "xng1vlan50" {
@@ -46,4 +43,61 @@ resource "xenorchestra_network" "xng1vlan998" {
 data "xenorchestra_template" "debian12base" {
   name_label = "debian12base_template"
   pool_id    = data.xenorchestra_pool.xng1.id
+}
+
+data "xenorchestra_template" "opnsense_template" {
+  name_label = "OPNsense_template"
+  pool_id    = data.xenorchestra_pool.xng1.id
+}
+
+# xng2 - m720q
+data "xenorchestra_pool" "m720q" {
+  name_label = "m720q"
+}
+
+data "xenorchestra_sr" "m720q" {
+  name_label = "Local storage"
+  pool_id    = data.xenorchestra_pool.m720q.id
+}
+
+data "xenorchestra_network" "m720q" {
+  name_label = "Pool-wide network associated with eth0"
+  pool_id    = data.xenorchestra_pool.m720q.id
+}
+
+resource "xenorchestra_network" "m720qvlan50" {
+  name_label        = "v.5G"
+  name_description  = "VLAN 50 - OPNsense LAN # tf-managed"
+  pool_id           = data.xenorchestra_pool.m720q.id
+  source_pif_device = "eth0"
+  mtu               = 9000
+  vlan              = 50
+}
+
+resource "xenorchestra_network" "m720qlan240" {
+  name_label        = "v.Guest"
+  name_description  = "VLAN 240 - Guest LAN # tf-managed"
+  pool_id           = data.xenorchestra_pool.m720q.id
+  source_pif_device = "eth0"
+  mtu               = 1500
+  vlan              = 240
+}
+
+resource "xenorchestra_network" "m720qvlan998" {
+  name_label        = "v.WAN1"
+  name_description  = "VLAN 998 - WAN1 # tf-managed"
+  pool_id           = data.xenorchestra_pool.m720q.id
+  source_pif_device = "eth0"
+  mtu               = 1500
+  vlan              = 998
+}
+
+data "xenorchestra_template" "debian12base" {
+  name_label = "debian12base_template"
+  pool_id    = data.xenorchestra_pool.m720q.id
+}
+
+data "xenorchestra_template" "opnsense_template" {
+  name_label = "OPNsense_template"
+  pool_id    = data.xenorchestra_pool.m720q.id
 }
